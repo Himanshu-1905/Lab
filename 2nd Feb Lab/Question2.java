3b4d51462e3a053f30be222f715a641b285a097d
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-02-02 18:00:42 +0530

    Add files via upload

---
+++ 2nd Feb Lab/Question2.java
@@ -0,0 +1,15 @@
+
+
+public class Question2 {
+	public static void main(String[] args) {
+	     
+		   int a, b, c;
+		   a = 15;
+		   b = 27;
+		   System.out.println("Before swapping : a, b = "+a+", "+ + b);
+		   c = a;
+		   a = b;
+		   b = c;   
+		  System.out.println("After swapping : a, b = "+a+", "+ + b);
+		 }
+}
---
+++ 2nd Feb Lab/Question3.java
@@ -0,0 +1,13 @@
+
+
+public class Question3 {
+	 
+    public static void main(String[] arg) {
+    	Float a = 25.5f;
+    	Float b = 3.5f;
+    	Float c = 4.5f;
+    	Float d = 40.5f;
+
+        System.out.println((a * b - b * b) / (d - c));
+    }
+}
