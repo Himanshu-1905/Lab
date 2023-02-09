0f8ce80b5e91a59912acacc41c85c118e8f1e05b
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-02-09 22:09:02 +0530

    Add files via upload

---
+++ 9th Feb Lab/ObjectCreation.java
@@ -0,0 +1,17 @@
+
+//Java program Creation of Object
+class ObjectCreation {
+ // Declaring and initializing string
+ // Custom input string
+ String name = "Himanshu";
+ 
+ public static void main(String[] args)
+ {
+     // creating object
+     // using new keyword
+     ObjectCreation obj = new ObjectCreation();
+
+     // Print and display the object
+     System.out.println(obj.name);
+ }
+}
